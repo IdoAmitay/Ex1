3e589dc4c2da52de1d8d0f9234abbc7df79c398d
Ido Amitay <atrap11@gmail.com>
2017-04-19 18:20:10 +0300

    after building server,main

--- Ex1/Ex1/Program.cs
+++ Ex1/Ex1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,16 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            IController controller = new MyController();
+            IModel model = new MyModel<MazeLib.Position>(controller);
             
+            ClientHandler ch = new ClientHandler(controller);
+            controller.SetModel(model);
+            controller.SetView(ch);
+            Client client = new Client();
+            Server server = new Server(8000, ch);
+            server.Start();
+            client.sendCommand("Generate");
         }
     }
 }
